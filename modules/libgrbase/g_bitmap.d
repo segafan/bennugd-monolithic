e703f036923c57a35d758e39815916ba8361517d
josebagar <josebagar@gmail.com>
2010-07-09 22:47:48 +0000

    * Added music transcoding script. * Integrated libgrbase.

---
+++ modules/libgrbase/g_bitmap.d
@@ -0,0 +1,31 @@
+../../../modules/libgrbase/g_bitmap.o: \
+ ../../../modules/libgrbase/g_bitmap.c \
+ ../../../modules/libgrbase/libgrbase.h \
+ ../../../modules/libgrbase/g_pal.h ../../../modules/libgrbase/g_bitmap.h \
+ ../../../modules/libgrbase/g_clear.h \
+ /home/joseba/cvs/bennugd/console/core/bgdrtm/../include/bgdcore.h \
+ ../../../modules/libgrbase/g_region.h \
+ ../../../modules/libgrbase/g_grlib.h \
+ ../../../modules/libgrbase/g_blendop.h \
+ ../../../modules/libgrbase/g_conversion.h \
+ ../../../modules/libgrbase/bitwise_map.h
+
+../../../modules/libgrbase/libgrbase.h:
+
+../../../modules/libgrbase/g_pal.h:
+
+../../../modules/libgrbase/g_bitmap.h:
+
+../../../modules/libgrbase/g_clear.h:
+
+/home/joseba/cvs/bennugd/console/core/bgdrtm/../include/bgdcore.h:
+
+../../../modules/libgrbase/g_region.h:
+
+../../../modules/libgrbase/g_grlib.h:
+
+../../../modules/libgrbase/g_blendop.h:
+
+../../../modules/libgrbase/g_conversion.h:
+
+../../../modules/libgrbase/bitwise_map.h:
