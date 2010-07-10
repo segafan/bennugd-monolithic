7a79e9f3ce35eb7f442474edf42b09d6b42d4057
josebagar <josebagar@gmail.com>
2010-07-10 23:36:26 +0000

    A bit of code cleanup.

---
+++ core/include/monolithic_includes.h
@@ -0,0 +1,62 @@
+/*
+ *  Copyright � 2006-2010 SplinterGU (Fenix/Bennugd)
+ *
+ *  This file is part of Bennu - Game Development
+ *
+ *  Bennu is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Bennu is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+
+#ifndef __MONOLITHIC_INCLUDES_H
+#define __MONOLITHIC_INCLUDES_H
+
+#include <libsdlhandler_symbols.h>
+#include <libjoy_symbols.h>
+#include <mod_say_symbols.h>
+#include <mod_string_symbols.h>
+#include <mod_math_symbols.h>
+#include <mod_time_symbols.h>
+#include <mod_file_symbols.h>
+#include <mod_sound_symbols.h>
+#include <mod_joy_symbols.h>
+#include <mod_proc_symbols.h>
+#include <mod_sort_symbols.h>
+#include <mod_timers_symbols.h>
+#include <mod_regex_symbols.h>
+#include <libgrbase_symbols.h>
+#include <libblit_symbols.h>
+#include <libvideo_symbols.h>
+#include <librender_symbols.h>
+#include <mod_video_symbols.h>
+#include <libmouse_symbols.h>
+#include <mod_mouse_symbols.h>
+#include <mod_map_symbols.h>
+#include <libfont_symbols.h>
+#include <mod_dir_symbols.h>
+#include <libtext_symbols.h>
+#include <mod_text_symbols.h>
+#include <mod_rand_symbols.h>
+#include <mod_grproc_symbols.h>
+#include <libscroll_symbols.h>
+#include <mod_scroll_symbols.h>
+#include <libkey_symbols.h>
+#include <mod_key_symbols.h>
+#include <mod_draw_symbols.h>
+#include <mod_screen_symbols.h>
+#include <mod_path_symbols.h>
+#include <mod_effects_symbols.h>
+#include <mod_blendop_symbols.h>
+
+#endif
