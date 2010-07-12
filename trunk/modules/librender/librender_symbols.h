7a4178152277d85f361aa34159b69b507ed9d10f
josebagar@gmail.com <josebagar@gmail.com@f91b80c7-bb6a-0374-6d28-0ee0fdcfb287>
2010-07-12 15:12:24 +0000

    Added missing files

---
+++ trunk/modules/librender/librender_symbols.h
@@ -0,0 +1,147 @@
+/*
+ *  Copyright © 2006-2010 SplinterGU (Fenix/Bennugd)
+ *  Copyright © 2002-2006 Fenix Team (Fenix)
+ *  Copyright © 1999-2002 José Luis Cebrián Pagüe (Fenix)
+ *
+ *  This file is part of Bennu - Game Development
+ *
+ *  Bennu is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Bennu is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+
+#ifndef __RENDER_SYMBOLS_H
+#define __RENDER_SYMBOLS_H
+
+#include <bgddl.h>
+#include "librender.h"
+
+#ifndef __BGDC__
+extern DLVARFIXUP __bgdexport( librender, globals_fixup )[];
+extern DLVARFIXUP __bgdexport( librender, locals_fixup )[];
+extern HOOK __bgdexport( librender, handler_hooks )[];
+extern void __bgdexport( librender, module_initialize )();
+extern void __bgdexport( librender, module_finalize )();
+extern void __bgdexport( librender, instance_create_hook )( INSTANCE * r );
+extern void __bgdexport( librender, instance_destroy_hook )( INSTANCE * r );
+#endif
+
+/* --------------------------------------------------------------------------- */
+
+char * __bgdexport( librender, modules_dependency )[] =
+{
+    "libgrbase",
+    "libvideo",
+    "libblit",
+    NULL
+};
+
+/* --------------------------------------------------------------------------- */
+
+DLCONSTANT __bgdexport( librender, constants_def )[] =
+{
+    { "C_SCREEN",           TYPE_DWORD, C_SCREEN },
+
+    { "PARTIAL_DUMP",       TYPE_DWORD, 0 },
+    { "COMPLETE_DUMP",      TYPE_DWORD, 1 },
+    { "NO_RESTORE",         TYPE_DWORD, -1 },
+    { "PARTIAL_RESTORE",    TYPE_DWORD, 0 },
+    { "COMPLETE_RESTORE",   TYPE_DWORD, 1 },
+
+    { "BACKGROUND",         TYPE_DWORD, 0 },
+    { "SCREEN",             TYPE_DWORD, -1 },
+
+    { "SCALE_SCALE2X",      TYPE_DWORD, SCALE_SCALE2X    },
+    { "SCALE_HQ2X",         TYPE_DWORD, SCALE_HQ2X       },
+    { "SCALE_SCANLINE2X",   TYPE_DWORD, SCALE_SCANLINE2X },
+    { "SCALE_NORMAL2X",     TYPE_DWORD, SCALE_NOFILTER   },
+    { "SCALE_NOFILTER",     TYPE_DWORD, SCALE_NOFILTER   },
+
+    { NULL          , 0         ,  0  }
+} ;
+
+/* --------------------------------------------------------------------------- */
+/* Definicion de variables globales (usada en tiempo de compilacion) */
+
+char __bgdexport( librender, globals_def )[] =
+
+    /* Frame */
+
+    "fps;\n"
+    "speed_gauge = 0;\n"
+    "FLOAT frame_time = 0;\n"
+
+    /* Screen */
+
+    "restore_type;\n"
+    "dump_type;\n"
+
+    /* Fade */
+
+    "fading;\n"
+    "alpha_steps = 16;\n"
+    ;
+
+/* --------------------------------------------------------------------------- */
+
+char __bgdexport( librender, locals_def )[] =
+
+    /* Render */
+
+    "ctype;\n"
+    "cnumber;\n"
+
+    "x;\n"
+    "y;\n"
+    "z;\n"
+    "file;\n"
+    "graph;\n"
+    "size=100;\n"
+    "angle;\n"
+    "flags;\n"
+    "alpha=255;\n"
+    "palette=0;\n"
+    "region;\n"
+    "resolution;\n"
+    "size_x=100;\n"
+    "size_y=100;\n"
+    "blendop=0;\n"
+    "pointer xgraph;\n"
+
+    "STRUCT _render_reserved_\n"
+    "object_id=0;\n"
+    "graph_ptr=0;\n"
+    "xgraph_flags;\n"
+    "STRUCT _saved_\n"
+    "x;\n"
+    "y;\n"
+    "z;\n"
+    "file;\n"
+    "graph;\n"
+    "size;\n"
+    "angle;\n"
+    "flags;\n"
+    "alpha;\n"
+    "palette;\n"
+    "size_x;\n"
+    "size_y;\n"
+    "blendop;\n"
+    "pointer xgraph;\n"
+    "centerx;\n"
+    "centery;\n"
+    "END\n"
+    "END\n"
+    ;
+
+#endif
---
+++ trunk/modules/mod_m7/mod_m7_symbols.h
@@ -0,0 +1,94 @@
+/*
+ *  Copyright © 2006-2010 SplinterGU (Fenix/Bennugd)
+ *  Copyright © 2002-2006 Fenix Team (Fenix)
+ *  Copyright © 1999-2002 José Luis Cebrián Pagüe (Fenix)
+ *
+ *  This file is part of Bennu - Game Development
+ *
+ *  Bennu is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Bennu is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ */
+
+#ifndef __MODM7_SYMBOLS_H
+#define __MODM7_SYMBOLS_H
+
+#include <bgddl.h>
+#include "mod_m7_defines.h"
+
+#ifndef __BGDC__
+extern DLVARFIXUP __bgdexport( mod_m7, globals_fixup )[];
+extern DLVARFIXUP __bgdexport( mod_m7, locals_fixup )[];
+extern void __bgdexport( mod_m7, module_initialize )();
+
+extern CONDITIONALLY_STATIC int modm7_start( INSTANCE * my, int * params );
+extern CONDITIONALLY_STATIC int modm7_stop( INSTANCE * my, int * params );
+#endif
+
+/* --------------------------------------------------------------------------- */
+/* Definicion de constantes (usada en tiempo de compilacion)         */
+
+DLCONSTANT __bgdexport( mod_m7, constants_def )[] =
+{
+    { "C_M7", TYPE_INT, C_M7 },
+    { NULL  , 0       , 0 }
+} ;
+
+/* --------------------------------------------------------------------------- */
+/* Definicion de variables globales (usada en tiempo de compilacion) */
+char __bgdexport( mod_m7, globals_def )[] =
+    "STRUCT m7[9]\n"
+    "camera;\n"
+    "height = 32;\n"
+    "distance = 64;\n"
+    "horizon = 0;\n"
+    "focus = 256;\n"
+    "z = 256;\n"
+    "color = 0;\n"
+    "flags = 0;\n"
+    "END\n" ;
+
+/* --------------------------------------------------------------------------- */
+/* Definicion de variables locales (usada en tiempo de compilacion)  */
+char __bgdexport( mod_m7, locals_def )[] =
+    "ctype;\n"
+    "cnumber;\n"
+    "height;\n"
+    "STRUCT _m7_reserved\n"
+    "  distance1;\n"
+    "END;\n";
+
+/* --------------------------------------------------------------------------- */
+
+DLSYSFUNCS  __bgdexport( mod_m7, functions_exports )[] =
+{
+    { "MODE7_START" , "IIIIII", TYPE_INT , SYSMACRO(modm7_start)  },
+    { "MODE7_STOP"  , "I"     , TYPE_INT , SYSMACRO(modm7_stop)   },
+    { "START_MODE7" , "IIIIII", TYPE_INT , SYSMACRO(modm7_start)  },
+    { "STOP_MODE7"  , "I"     , TYPE_INT , SYSMACRO(modm7_stop)   },
+
+    { NULL          , NULL    , 0        , NULL         }
+};
+
+/* --------------------------------------------------------------------------- */
+
+char * __bgdexport( mod_m7, modules_dependency )[] =
+{
+    "libgrbase",
+    "libvideo",
+    "librender",
+    NULL
+};
+
+#endif
