038370877a8849f636063ce8343cacc2126a75f7
josebagar <josebagar@gmail.com>
2010-08-27 22:54:18 +0000

    The transcoder now informs you when transcoding is available (if notify-send is found).

--- transcode_music.sh
+++ transcode_music.sh
@@ -15,6 +15,8 @@ CHANNELS=2;		# 1 for mono, 2 for stereo
 FREQ=48000;		# Leave this @ 48KHz
 OUTOGG=converted_music;
 OUTWAV=converted_wav;
+NOTIFIER=$(which notify-send);
+N=0;
 
 cd $(dirname 0)
 
@@ -30,22 +32,34 @@ for i in *.{ogg,mp3,wma}; do
 			echo ==================================================
 			echo
 			$VLC $i --sout="#transcode{acodec=vorb,ab=$RATE,channels=$CHANNELS,samplerate=$FREQ}:duplicate{dst=std{access=file,mux=ogg,dst='$OUTOGG/$i'}" "vlc://quit";
+			N=$((N+1))
 		fi
 	fi
 done
 
 # Try to recode WAV files
 for i in *.wav; do
-        if [ -f $i ]; then
-                if [ ! -f $OUTWAV/$i ]; then
-        		if [ ! -d $OUTWAV ]; then
-                		mkdir $OUTWAV;
-		        fi
+	if [ -f $i ]; then
+		if [ ! -f $OUTWAV/$i ]; then
+			if [ ! -d $OUTWAV ]; then
+				mkdir $OUTWAV;
+			fi
 			echo
-                        echo Converting $i into $OUTWAV/$i
-                        echo ==================================================
+			echo Converting $i into $OUTWAV/$i
+			echo ==================================================
 			echo
 			$VLC $i --sout="#transcode{vcodec=none,acodec=s16l,ab=$RATE,channels=$CHANNELS,samplerate=$FREQ}:duplicate{dst=std{access=file,mux=wav,dst='$OUTWAV/$i'}" "vlc://quit";
-                fi
+			N=$((N+1))
+			fi
         fi
 done
+
+# Try to let the user know I've finished transcoding the music
+if [ -x $NOTIFIER ]; then
+    if [ $N -eq 0 ]; then
+		$NOTIFIER -i dialog-information "No files transcoded"
+	else
+		$NOTIFIER -i dialog-information "$N files transcoded"
+	fi
+fi
+
