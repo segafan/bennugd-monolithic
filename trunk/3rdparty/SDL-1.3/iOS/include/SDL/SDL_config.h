bd26fa7065b8f9c2ccc03df82c59915dfd68e372
josebagar@gmail.com <josebagar@gmail.com@f91b80c7-bb6a-0374-6d28-0ee0fdcfb287>
2011-03-25 16:44:46 +0000

    Added dependencies and changes to makefiles to allow modules playback through SDL_mixer.

--- trunk/3rdparty/SDL-1.3/iOS/include/SDL/SDL_config.h
+++ trunk/3rdparty/SDL-1.3/iOS/include/SDL/SDL_config.h
@@ -34,8 +34,8 @@
 /* Add any platform that doesn't build using the configure system. */
 #if defined(__WIN32__)
 #include "SDL_config_windows.h"
-#elif defined(__MACOSX__)
-#include "SDL_config_macosx.h"
+/*#elif defined(__MACOSX__)
+#include "SDL_config_macosx.h"*/
 #elif defined(__IPHONEOS__) 
 #include "SDL_config_iphoneos.h"
 #elif defined(__ANDROID__)
