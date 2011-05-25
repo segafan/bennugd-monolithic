3f701757f96389fe4b10344fb3adf6e9c6d46c43
josebagar@gmail.com <josebagar@gmail.com@f91b80c7-bb6a-0374-6d28-0ee0fdcfb287>
2011-05-25 21:52:49 +0000

    SDL 1.3 Update for OSX Small iOS resolution fixes (force FRAMELESS always)

--- trunk/modules/libvideo/g_video.c
+++ trunk/modules/libvideo/g_video.c
@@ -5,19 +5,24 @@
  *
  *  This file is part of Bennu - Game Development
  *
- *  Bennu is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This software is provided 'as-is', without any express or implied
+ *  warranty. In no event will the authors be held liable for any damages
+ *  arising from the use of this software.
  *
- *  Bennu is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  Permission is granted to anyone to use this software for any purpose,
+ *  including commercial applications, and to alter it and redistribute it
+ *  freely, subject to the following restrictions:
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *     1. The origin of this software must not be misrepresented; you must not
+ *     claim that you wrote the original software. If you use this software
+ *     in a product, an acknowledgment in the product documentation would be
+ *     appreciated but is not required.
+ *
+ *     2. Altered source versions must be plainly marked as such, and must not be
+ *     misrepresented as being the original software.
+ *
+ *     3. This notice may not be removed or altered from any source
+ *     distribution.
  *
  */
 
@@ -76,53 +81,53 @@ int scale_resolution_aspectratio_offy = 0;
 
 DLCONSTANT  __bgdexport( libvideo, constants_def )[] =
 {
-    { "M320X200",           TYPE_DWORD, 3200200             },
-    { "M320X240",           TYPE_DWORD, 3200240             },
-    { "M320X400",           TYPE_DWORD, 3200400             },
-    { "M360X240",           TYPE_DWORD, 3600240             },
-    { "M376X282",           TYPE_DWORD, 3760282             },
-    { "M400X300",           TYPE_DWORD, 4000300             },
-    { "M512X384",           TYPE_DWORD, 5120384             },
-    { "M640X400",           TYPE_DWORD, 6400400             },
-    { "M640X480",           TYPE_DWORD, 6400480             },
-    { "M800X600",           TYPE_DWORD, 8000600             },
-    { "M1024X768",          TYPE_DWORD, 10240768            },
-    { "M1280X1024",         TYPE_DWORD, 12801024            },
+    { "M320X200"            , TYPE_DWORD    , 3200200               },
+    { "M320X240"            , TYPE_DWORD    , 3200240               },
+    { "M320X400"            , TYPE_DWORD    , 3200400               },
+    { "M360X240"            , TYPE_DWORD    , 3600240               },
+    { "M376X282"            , TYPE_DWORD    , 3760282               },
+    { "M400X300"            , TYPE_DWORD    , 4000300               },
+    { "M512X384"            , TYPE_DWORD    , 5120384               },
+    { "M640X400"            , TYPE_DWORD    , 6400400               },
+    { "M640X480"            , TYPE_DWORD    , 6400480               },
+    { "M800X600"            , TYPE_DWORD    , 8000600               },
+    { "M1024X768"           , TYPE_DWORD    , 10240768              },
+    { "M1280X1024"          , TYPE_DWORD    , 12801024              },
 
-    { "MODE_WINDOW",        TYPE_DWORD, MODE_WINDOW         },
-    { "MODE_2XSCALE",       TYPE_DWORD, MODE_2XSCALE        },
-    { "MODE_FULLSCREEN",    TYPE_DWORD, MODE_FULLSCREEN     },
-    { "MODE_DOUBLEBUFFER",  TYPE_DWORD, MODE_DOUBLEBUFFER   },
-    { "MODE_HARDWARE",      TYPE_DWORD, MODE_HARDWARE       },
+    { "MODE_WINDOW"         , TYPE_DWORD    , MODE_WINDOW           },
+    { "MODE_2XSCALE"        , TYPE_DWORD    , MODE_2XSCALE          },
+    { "MODE_FULLSCREEN"     , TYPE_DWORD    , MODE_FULLSCREEN       },
+    { "MODE_DOUBLEBUFFER"   , TYPE_DWORD    , MODE_DOUBLEBUFFER     },
+    { "MODE_HARDWARE"       , TYPE_DWORD    , MODE_HARDWARE         },
 
-    { "MODE_WAITVSYNC",     TYPE_DWORD, MODE_WAITVSYNC      },
-    { "WAITVSYNC",          TYPE_DWORD, MODE_WAITVSYNC      },
+    { "MODE_WAITVSYNC"      , TYPE_DWORD    , MODE_WAITVSYNC        },
+    { "WAITVSYNC"           , TYPE_DWORD    , MODE_WAITVSYNC        },
 
-    { "DOUBLE_BUFFER",      TYPE_DWORD, MODE_DOUBLEBUFFER   },  /* Obsolete */
-    { "HW_SURFACE",         TYPE_DWORD, MODE_HARDWARE       },  /* Obsolete */
+    { "DOUBLE_BUFFER"       , TYPE_DWORD    , MODE_DOUBLEBUFFER     },  /* Obsolete */
+    { "HW_SURFACE"          , TYPE_DWORD    , MODE_HARDWARE         },  /* Obsolete */
 
-    { "MODE_8BITS",         TYPE_DWORD, 8                   },
-    { "MODE_16BITS",        TYPE_DWORD, 16                  },
-    { "MODE_32BITS",        TYPE_DWORD, 32                  },
+    { "MODE_8BITS"          , TYPE_DWORD    , 8                     },
+    { "MODE_16BITS"         , TYPE_DWORD    , 16                    },
+    { "MODE_32BITS"         , TYPE_DWORD    , 32                    },
 
-    { "MODE_8BPP",          TYPE_DWORD, 8                   },
-    { "MODE_16BPP",         TYPE_DWORD, 16                  },
-    { "MODE_32BPP",         TYPE_DWORD, 32                  },
+    { "MODE_8BPP"           , TYPE_DWORD    , 8                     },
+    { "MODE_16BPP"          , TYPE_DWORD    , 16                    },
+    { "MODE_32BPP"          , TYPE_DWORD    , 32                    },
 
-    { "MODE_MODAL",         TYPE_DWORD, MODE_MODAL          },  /* GRAB INPU */
-    { "MODE_FRAMELESS",     TYPE_DWORD, MODE_FRAMELESS      },  /* FRAMELESS window */
+    { "MODE_MODAL"          , TYPE_DWORD    , MODE_MODAL            },  /* GRAB INPU */
+    { "MODE_FRAMELESS"      , TYPE_DWORD    , MODE_FRAMELESS        },  /* FRAMELESS window */
 
-    { "SCALE_NONE",         TYPE_DWORD, SCALE_NONE          },
-	
-	{ "SRO_NORMAL",         TYPE_DWORD, SRO_NORMAL          },
-	{ "SRO_LEFT",           TYPE_DWORD, SRO_LEFT            },
-	{ "SRO_DOWN",           TYPE_DWORD, SRO_DOWN            },
-	{ "SRO_RIGHT",          TYPE_DWORD, SRO_RIGHT           },
+    { "SCALE_NONE"          , TYPE_DWORD    , SCALE_NONE            },
 
-	{ "SRA_STRETCH",        TYPE_DWORD, SRA_STRETCH         },
-	{ "SRA_PRESERVE",       TYPE_DWORD, SRA_PRESERVE        },
+    { "SRO_NORMAL"          , TYPE_DWORD    , SRO_NORMAL            },
+    { "SRO_LEFT"            , TYPE_DWORD    , SRO_LEFT              },
+    { "SRO_DOWN"            , TYPE_DWORD    , SRO_DOWN              },
+    { "SRO_RIGHT"           , TYPE_DWORD    , SRO_RIGHT             },
 
-    { NULL          , 0         ,  0  }
+    { "SRA_STRETCH"         , TYPE_DWORD    , SRA_STRETCH           },
+    { "SRA_PRESERVE"        , TYPE_DWORD    , SRA_PRESERVE          },
+
+    { NULL                  , 0             , 0                     }
 } ;
 
 /* --------------------------------------------------------------------------- */
@@ -361,6 +366,9 @@ int gr_set_mode( int width, int height, int depth )
     /* Setup the SDL Video Mode */
 
     sdl_flags = SDL_HWPALETTE;
+#ifdef TARGET_IOS
+    sdl_flags |= SDL_NOFRAME;
+#endif
     if ( double_buffer ) sdl_flags |= SDL_DOUBLEBUF;
     if ( full_screen ) sdl_flags |= SDL_FULLSCREEN;
     if ( frameless ) sdl_flags |= SDL_NOFRAME;
@@ -372,7 +380,6 @@ int gr_set_mode( int width, int height, int depth )
 
     if ( scale_resolution )
     {
-        printf("Scale_resolution ON\n");
         switch ( scale_resolution_orientation )
         {
             case    SRO_LEFT:
@@ -404,9 +411,9 @@ int gr_set_mode( int width, int height, int depth )
         double  fw = 0.0, fh = 0.0, fx = 0.0, fy = 0.0;
         int     h, w;
         int     start_w = 0, start_h = 0, fix = 1;
-		
-		scale_resolution_aspectratio_offx = 0;
-		scale_resolution_aspectratio_offy = 0;
+
+        scale_resolution_aspectratio_offx = 0;
+        scale_resolution_aspectratio_offy = 0;
 
         switch ( scale_resolution_orientation )
         {
@@ -495,11 +502,7 @@ int gr_set_mode( int width, int height, int depth )
     }
     else
     {
-        printf("Scale_resolution OFF\n");
-        //screen = SDL_SetVideoMode( surface_width, surface_height, depth, sdl_flags );
-        //printf("Setting mode %dx%dx%d\n (Flags: %d)\n", surface_width, surface_height, depth, sdl_flags);
-        screen = SDL_SetVideoMode(0, 0, 16, 0);
-        printf("Video resolution set to %dx%dx%d\n", screen->w, screen->h, screen->format->BitsPerPixel);
+        screen = SDL_SetVideoMode( surface_width, surface_height, depth, sdl_flags );
     }
 
     if ( !screen ) return -1;
