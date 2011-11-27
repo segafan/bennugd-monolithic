a2a0cbd7c5e84f6edb2e10c8ca2f7a3ed3b2db56
josebagar@gmail.com <josebagar@gmail.com@f91b80c7-bb6a-0374-6d28-0ee0fdcfb287>
2011-11-27 03:09:15 +0000

    Greatly expanded mod_curl binding

--- trunk/modules/mod_curl/main.c
+++ trunk/modules/mod_curl/main.c
@@ -25,27 +25,51 @@
 #include <stdio.h>
 #include <bgddl.h>
 #include <xstrings.h>
+#include <bgdrtm.h>
 #include <curl/curl.h>
 #include <SDL.h>
 
-// Status vars
-// This code really needs some cleanup to allow multiple transfers
-// at a time
-static int transfer_in_progress=0;
-static int transfer_done=0;
-static int transfer_failed=0;
+#ifndef MAX_DOWNLOADS
+#define MAX_DOWNLOADS 16
+#endif
 
-/* --------------------------------------------------------------------------- */
-/* --------------------------------------------------------------------------- */
 /* --------------------------------------------------------------------------- */
 
 typedef struct
 {
-    char *src;
-    char *dst;
+    CURL *curl;
+    struct curl_httppost *formpost;
+    struct curl_httppost *lastptr;
+    FILE *outfd;        // Used when downloading to a file
+    int used;
+} curl_info ;
+
+typedef struct
+{
     int  *id;
+    int  *status;
     int ( *fn )();
 } bgdata ;
+
+curl_info download_info[MAX_DOWNLOADS];
+
+// Find the first free curl_info index, return numeric index
+int findindex() {
+    int i=0;
+    
+    for (i=0; i<MAX_DOWNLOADS; i++) {
+        if (download_info[i].used == 0) {
+            // Initialize values, just in case they haven't been already
+            download_info[i].formpost = NULL;
+            download_info[i].lastptr  = NULL;
+            download_info[i].outfd    = NULL;
+            download_info[i].used     = 1;
+            return i;
+        }
+    }
+    
+    return -1;
+}
 
 /* --------------------------------------------------------------------------- */
 
@@ -57,11 +81,9 @@ typedef struct
 static bgdata *prep( int *params )
 {
     bgdata *t = ( bgdata* )malloc( sizeof( bgdata ) );
-    t->src = ( char * )string_get( params[0] );
-    t->dst = ( char * )string_get( params[1] );
-    t->id = ( int* )params[2];
-    string_discard( params[0] );
-    string_discard( params[1] );
+
+    t->id     = ( int* )params[0];
+    t->status = ( int* )params[1];
 
     return t;
 }
@@ -75,59 +97,163 @@ static bgdata *prep( int *params )
 static int bgDoLoad( void *d )
 {
     bgdata *t  = ( bgdata* )d;
-    *( t->id ) = -2 ;
-    *( t->id ) = ( *t->fn )( t->src, t->dst );
+    *( t->status ) = -2 ;
+    *( t->status ) = ( *t->fn )( t->id );
     free( t );
     return 0;
 }
 
-// Actual transfer function
-int curl_get(char *src, char *dst) {
-    CURL *curl;
-    FILE *fd;
+// Maps curl_global_init
+static int bgd_curl_global_init(INSTANCE * my, int * params) {
+    return curl_global_init(CURL_GLOBAL_ALL);
+}
+
+// Maps curl_global_cleanup
+static int bgd_curl_global_cleanup(INSTANCE * my, int * params) {
+    curl_global_cleanup();
     
-    // First, get the file descriptor for the output file
-    fd = fopen(dst, "w");
-    // Check file opening
-    if (fd == NULL)
+    return 0;
+}
+
+// Maps curl_formadd
+static int bgd_curl_formadd(INSTANCE * my, int * params) {
+    int retval = 0;
+
+    if(params[0] == -1 || params[0] > MAX_DOWNLOADS)
         return -1;
     
-    // Set up some download variables
-    curl = curl_easy_init();
+    // Actually perform curl_formadd
+    retval = curl_formadd(&download_info[params[0]].formpost,
+                 &download_info[params[0]].lastptr,
+                 params[1], string_get(params[2]),
+                 params[3], string_get(params[4]),
+                 CURLFORM_END);
+    
+    string_discard(params[2]);
+    string_discard(params[4]);
+    
+    return retval;
+}
 
-    if(curl == NULL)
+// Maps curl_formfree
+static int bgd_curl_formfree(INSTANCE * my, int * params) {
+    if(params[0] == -1 || params[0] > MAX_DOWNLOADS)
+        return -1;
+    
+    // Actually perform curl_formfree
+    curl_formfree(download_info[params[0]].formpost);
+    
+    return 0;
+}
+
+// Maps curl_easy_init
+static int bgd_curl_easy_init(INSTANCE * my, int * params) {
+    int i;
+    
+    // Get the index of the connection
+    if( (i = findindex()) == -1)
+        return -1;
+    
+    download_info[i].curl = curl_easy_init();
+    
+    if (download_info[i].curl == NULL)
         return -1;
 
-    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fd);
-    curl_easy_setopt(curl, CURLOPT_URL, src);
+    return i;
+}
+
+// Maps curl_easy_cleanup
+static int bgd_curl_easy_cleanup(INSTANCE * my, int * params) {
+    if(params[0] == -1 || params[0] > MAX_DOWNLOADS)
+        return -1;
     
-    // Perform download, this function won't quit until it's done
-    curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-
-    fclose(fd);
-
-    transfer_in_progress = 0;
-    transfer_done = 1;
+    download_info[params[0]].used = 0;
+    
+    curl_easy_cleanup(download_info[params[0]].curl);
 
     return 0;
 }
 
-// Start a new download in a new thread
-static int bgd_curl_get(INSTANCE * my, int * params) {
-    if(transfer_in_progress == 1)
+// Maps curl_easy_setopt
+static int bgd_curl_easy_setopt(INSTANCE * my, int * params) {
+    if(params[0] == -1 || params[0] > MAX_DOWNLOADS)
         return -1;
     
-    bgdata *t = prep( params );
-    t->fn = curl_get;
+    CURLcode retval;
     
-    // Reset values
-    transfer_done   = 0;
-    transfer_failed = 0;
+    // Actually perform curl_easy_setopt
+    switch (params[1]) {
+        case CURLOPT_HTTPPOST:
+            retval = curl_easy_setopt(download_info[params[0]].curl,
+                                      CURLOPT_HTTPPOST,
+                                      download_info[params[0]].formpost);
+            break;
+
+        default:
+            retval = curl_easy_setopt(download_info[params[0]].curl,
+                                      params[1],
+                                      params[2]);
+            break;
+    }
+    
+    return (int)retval;
+}
+
+static int bgd_curl_easy_setopt2(INSTANCE * my, int * params) {
+    if(params[0] == -1 || params[0] > MAX_DOWNLOADS)
+        return -1;
+    
+    CURLcode retval;
+    
+    // Actually perform curl_easy_setopt
+    switch (params[1]) {
+            // Handle some special cases
+        case CURLOPT_WRITEDATA:
+            // Point the output file pointer to the given location
+            download_info[params[0]].outfd = fopen(string_get(params[2]), "w");
+            string_discard(params[2]);
+            if(download_info[params[0]].outfd == NULL)
+                return -1;
+            
+            retval = curl_easy_setopt(download_info[params[0]].curl,
+                                      CURLOPT_WRITEDATA,
+                                      download_info[params[0]].outfd);
+            break;
+            
+        default:
+            retval = curl_easy_setopt(download_info[params[0]].curl,
+                                      params[1],
+                                      string_get(params[2]));
+            string_discard(params[2]);
+            break;
+    }
+    
+    return (int)retval;
+}
+
+// Actual perform function
+int curl_perform(int id) {
+    if(download_info[id].curl == NULL)
+        return -1;
+    
+    // Perform download, this function won't quit until it's done
+    curl_easy_perform(download_info[id].curl);
+    
+    // If downloading to a file, close its file descriptor
+    if (download_info[id].outfd != NULL) {
+        fclose(download_info[id].outfd);
+    }
+    
+    return 0;
+}
+
+// Map curl_easy_perform
+static int bgd_curl_easy_perform(INSTANCE * my, int * params) {
+    bgdata *t = prep( params );
+    t->fn = curl_perform;
     
 #if SDL_VERSION_ATLEAST(1,3,0)
-    SDL_CreateThread( bgDoLoad, "CURL download thread", (void *)t );
+    SDL_CreateThread( bgDoLoad, "CURL perform thread", (void *)t );
 #else
     SDL_CreateThread( bgDoLoad, (void *)t );
 #endif
@@ -137,6 +263,14 @@ static int bgd_curl_get(INSTANCE * my, int * params) {
 
 DLSYSFUNCS __bgdexport( mod_curl, functions_exports )[] =
 {
-    { "CURL_GET"            , "SSP" , TYPE_INT,   bgd_curl_get       },
-    { 0                     , 0     , 0          , 0}
+    { "CURL_GLOBAL_INIT"    , ""      , TYPE_INT , bgd_curl_global_init    },
+    { "CURL_GLOBAL_CLEANUP" , ""      , TYPE_INT , bgd_curl_global_cleanup },
+    { "CURL_INIT"           , ""      , TYPE_INT , bgd_curl_easy_init      },
+    { "CURL_CLEANUP"        , "I"     , TYPE_INT , bgd_curl_easy_cleanup   },
+    { "CURL_FORMADD"        , "IISIS" , TYPE_INT , bgd_curl_formadd        },
+    { "CURL_FORMFREE"       , "I"     , TYPE_INT , bgd_curl_formfree       },
+    { "CURL_SETOPT"         , "III"   , TYPE_INT , bgd_curl_easy_setopt    },
+    { "CURL_SETOPT"         , "IIS"   , TYPE_INT , bgd_curl_easy_setopt2   },
+    { "CURL_PERFORM"        , "IP"    , TYPE_INT , bgd_curl_easy_perform   },
+    { 0                     , 0       , 0        , 0                       }
 };
