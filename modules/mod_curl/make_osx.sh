87e656b8c105846daaf26b4c0484495ff53e995f
josebagar <josebagar@gmail.com>
2012-01-24 22:41:24 +0000

    Syncing with SDL2

--- modules/mod_curl/make_osx.sh
+++ modules/mod_curl/make_osx.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-gcc -O2 -m32 -Wall -I../bennugd/base/core/include/ -I/usr/local/include/ -I/opt/local/include/SDL/ -D_REENTRANT -c main.c
-if [ -f main.o ]; then
-        gcc main.o -O2 -dynamiclib -m32 -o mod_curl.dylib -L../bennugd_bin/lib/ -lbgdrtm -lcurl -L/opt/local/lib -lSDL -install_name mod_curl.dylib -Wl,-single_module
+gcc -O2 -m32 -Wall -I../../core/include/ -I/usr/local/include/ -I../../core/bgdrtm/include/ -I/opt/local/include/SDL/ -D_REENTRANT -c mod_curl.c
+if [ -f mod_curl.o ]; then
+        gcc mod_curl.o -O2 -dynamiclib -m32 -o mod_curl.dylib -L../../../../bennugd_bin/lib/ -lbgdrtm -lcurl -L/opt/local/lib -lSDL -install_name mod_curl.dylib -Wl,-single_module
 fi
 
 if [ -f mod_curl.dylib ]; then
-        rm main.o
+        rm mod_curl.o
 fi
