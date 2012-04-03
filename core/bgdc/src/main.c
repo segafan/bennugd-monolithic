0ec23d050eb201d202f5511812ab425524c29ad8
josebagar <josebagar@gmail.com>
2012-04-03 21:28:14 +0000

    Updated copyright dates (was about time)

--- core/bgdc/src/main.c
+++ core/bgdc/src/main.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright © 2006-2011 SplinterGU (Fenix/Bennugd)
+ *  Copyright © 2006-2012 SplinterGU (Fenix/Bennugd)
  *  Copyright © 2002-2006 Fenix Team (Fenix)
  *  Copyright © 1999-2002 José Luis Cebrián Pagüe (Fenix)
  *
@@ -167,7 +167,7 @@ int main( int argc, char *argv[] )
     strncpy( appexepath, appexefullpath, ptr - appexefullpath );
 
     printf( BGDC_VERSION "\n"
-            "Copyright © 2006-2011 SplinterGU (Fenix/BennuGD)\n"
+            "Copyright © 2006-2012 SplinterGU (Fenix/BennuGD)\n"
             "Copyright © 2002-2006 Fenix Team (Fenix)\n"
             "Copyright © 1999-2002 José Luis Cebrián Pagüe (Fenix)\n"
             "Bennu Game Development comes with ABSOLUTELY NO WARRANTY;\n"
