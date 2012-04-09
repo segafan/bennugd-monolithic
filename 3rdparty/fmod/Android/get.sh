f6168a9f4707073d4742852bcd155b3108001003
josebagar <josebagar@gmail.com>
2012-04-09 02:41:27 +0000

    * Updated fmod download script * Updated the Androd fmod project.

--- 3rdparty/fmod/Android/get.sh
+++ 3rdparty/fmod/Android/get.sh
@@ -4,10 +4,11 @@
 #
 # If you find that this script doesn't download the latest stable release
 # of FMOD Ex, please drop me a line and I'll update it.
-wget -c http://www.fmod.org/index.php/release/version/fmodapi44000android.tar.gz -O fmodapi.tar.gz
+wget -c http://www.fmod.org/index.php/release/version/fmodapi44004android.tar.gz -O fmodapi.tar.gz
 
 # Extract the things we need from the archive, dump the rest
 tar xvzf fmodapi.tar.gz
-cp -r "fmodapi44000android/api/inc" .
-cp -r "fmodapi44000android/api/lib" .
-rm -rf fmodapi44000android fmodapi.tar.gz
+cp -r fmodapi44004android/api/inc .
+cp -r fmodapi44004android/api/lib .
+cp -r fmodapi44004android/api/lib/* ../../../projects/android_fmod/libs/
+rm -rf fmodapi44004android fmodapi.tar.gz
---
+++ projects/android_fmod/jni/curl/Android.mk
@@ -0,0 +1,1 @@
+include $(LOCAL_PATH)/../../../../3rdparty/curl/Android.mk
