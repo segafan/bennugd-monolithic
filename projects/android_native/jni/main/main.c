61aea91b1b5c82aa0051452c0e6c571853715dc3
josebagar <josebagar@gmail.com>
2012-07-17 23:12:46 +0000

    * Modified the android fmod downloader a bit * Created a template for the android antive activity

--- projects/android_native/jni/main/main.c
+++ projects/android_native/jni/main/main.c
@@ -1,138 +1,80 @@
 /*
- *  Copyright © 2006-2011 SplinterGU (Fenix/Bennugd)
- *  Copyright © 2002-2006 Fenix Team (Fenix)
- *  Copyright © 1999-2002 José Luis Cebrián Pagüe (Fenix)
- *
- *  This file is part of Bennu - Game Development
- *
- *  This software is provided 'as-is', without any express or implied
- *  warranty. In no event will the authors be held liable for any damages
- *  arising from the use of this software.
- *
- *  Permission is granted to anyone to use this software for any purpose,
- *  including commercial applications, and to alter it and redistribute it
- *  freely, subject to the following restrictions:
- *
- *     1. The origin of this software must not be misrepresented; you must not
- *     claim that you wrote the original software. If you use this software
- *     in a product, an acknowledgment in the product documentation would be
- *     appreciated but is not required.
- *
- *     2. Altered source versions must be plainly marked as such, and must not be
- *     misrepresented as being the original software.
- *
- *     3. This notice may not be removed or altered from any source
- *     distribution.
- *
+ Simple DirectMedia Layer
+ Copyright (C) 1997-2012 Sam Lantinga <slouken@libsdl.org>
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
  */
 
-/*
- * INCLUDES
+#include <android/log.h>
+#include <android_native_app_glue.h>
+#include <stdlib.h>
+
+
+/**
+ * Process the next input event.
  */
+static void handle_input(struct android_app* app, AInputEvent* event) {
+}
 
-#include "SDL.h"
-#include <stdio.h>
-
-#include "bgdrtm.h"
-
-/* ---------------------------------------------------------------------- */
-
-static char * dcb_exts[] = { ".dcb", ".dat", ".bin", NULL };
-
-static int standalone  = 0;  /* 1 only if this is an standalone interpreter   */
-static int embedded    = 0;  /* 1 only if this is a stub with an embedded DCB */
-
-/* ---------------------------------------------------------------------- */
-
-/*
- *  FUNCTION : main
- *
- *  Main function
- *
- *  PARAMS:
- *      INT n: ERROR LEVEL to return to OS
- *
- *  RETURN VALUE:
- *      No value
- *
+/**
+ * Process the next main command.
  */
+static void handle_cmd(struct android_app* app, int32_t cmd) {
+    switch (cmd) {
+        case APP_CMD_SAVE_STATE:
+            // The system has asked us to save our current state.  Do so.
+            break;
+        case APP_CMD_INIT_WINDOW:
+            // The window is being shown, get it ready.
+            break;
+        case APP_CMD_TERM_WINDOW:
+            // The window is being hidden or closed, clean it up.
+            break;
+        case APP_CMD_GAINED_FOCUS:
+            // When our app gains focus, we start monitoring the accelerometer.
+            break;
+        case APP_CMD_LOST_FOCUS:
+            // When our app loses focus, we stop monitoring the accelerometer.
+            // This is to avoid consuming battery while not being used.
+            break;
+    }
+}
 
-int main( int argc, char *argv[] )
+/* This'll be the main entry point for our app */
+void android_main(struct android_app *state)
 {
-    char * filename = NULL, dcbname[ __MAX_PATH ], *ptr ;
-    int i, j, ret = -1;
-    file * fp = NULL;
-    INSTANCE * mainproc_running;
-    dcb_signature dcb_signature;
+	app_dummy(); // Make sure glue isn't stripped
+    state->onAppCmd = handle_cmd;
+    state->onInputEvent = handle_input;
     
-    SDL_Log ("BennuGD init\n");
-    
-    filename = "main.dcb";
-	if(file_exists("main.dcb"))
-        SDL_Log("main.dcb exists in APK\n");
-    else {
-        SDL_Log("main.dcb doesn't exist in APK, quitting\n");
-        return 1;
-    }
-    
-    // Remember to compile DCB with debug (bgdc -g) info!
-    debug = 1;
-	
-    /* Initialization (modules needed before dcb_load) */
-	
-    string_init() ;
-    init_c_type() ;
-	
-    /* Init application title for windowed modes */
-	
-    strcpy( dcbname, filename ) ;
-    
-    SDL_Log("Loading main.dcb...\n");
-	
-    /* First try to load directly (we expect myfile.dcb) */
-    if ( !dcb_load( dcbname ) )
+    while (!state->destroyRequested)
     {
-        char ** dcbext = dcb_exts;
-        int dcbloaded = 0;
-        
-        while ( dcbext && *dcbext )
+        int events;
+        struct android_poll_source *source;
+
+        // Main Loop
+        while ((ALooper_pollAll(0, NULL, &events, (void**)&source)) >= 0)
         {
-            strcpy( dcbname, filename ) ;
-            strcat( dcbname, *dcbext ) ;
-            if (( dcbloaded = dcb_load( dcbname ) ) ) break;
-            dcbext++;
+            if (source)
+            {
+                source->process(state, source);
+            }
         }
         
-        if ( !dcbloaded )
-        {
-            SDL_Log( "%s: doesn't exist or isn't version %d DCB compatible\n", filename, DCB_VERSION >> 8 ) ;
-            return -1 ;
-        }
+        // Do other needed stuff here
     }
-	
-    /* If the dcb is not in debug mode */
-	
-    if ( dcb.data.NSourceFiles == 0 ) debug = 0;
-	
-    /* Initialization (modules needed after dcb_load) */
-	
-    sysproc_init() ;
-	
-    argv[0] = filename;
-    bgdrtm_entry( argc, argv );
-	
-    if ( mainproc )
-    {
-        mainproc_running = instance_new( mainproc, NULL ) ;
-        ret = instance_go_all() ;
-    }
-	
-    bgdrtm_exit( ret );
-	
-    free( appexename        );
-    free( appexepath        );
-    free( appexefullpath    );
-    free( appname           );
-	
-    return ret;
 }
