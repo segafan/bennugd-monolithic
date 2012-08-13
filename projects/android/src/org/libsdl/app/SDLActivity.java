b829a3e40b764dfcf6b58413a7ec31b28c5aad0d
josebagar <josebagar@gmail.com>
2012-08-13 14:37:52 +0000

    * [Android] Volume keys are now honored

--- projects/android/src/org/libsdl/app/SDLActivity.java
+++ projects/android/src/org/libsdl/app/SDLActivity.java
@@ -553,6 +553,12 @@ class SDLSurface extends SurfaceView implements SurfaceHolder.Callback,
 
     // Key events
     public boolean onKey(View  v, int keyCode, KeyEvent event) {
+        
+        // Ignore volumen keys so they're handled by Android
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN ||
+            keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+			return false;
+		}
 
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
             //Log.v("SDL", "key down: " + keyCode);
