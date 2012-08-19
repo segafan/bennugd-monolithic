c1be9092057e4be6ececa69ddd6ee16e46b844bf
josebagar@gmail.com <josebagar@gmail.com@f91b80c7-bb6a-0374-6d28-0ee0fdcfb287>
2012-08-19 00:26:05 +0000

    * The packager script is now working fine in OSX (including icons)

--- packager/pack_packager.sh
+++ packager/pack_packager.sh
@@ -19,11 +19,27 @@ function package_osx {
         fi
     done
     
-    # Finally, patch main.py to point to python.org binaries
-    # This is CRAP! but I cannot get cx_freeze to work on Mac
+    # Patch main.py to point to python.org binaries
+    # THIS IS CRAP! but I cannot get cx_freeze to work on Mac
     sed -i '' 's/\/usr\/bin\/env python/\/Library\/Frameworks\/Python.framework\/Versions\/2.7\/bin\/python/g' main.py
     
+    # Finally, create DMG image with the app (We have to do this many times to be able to set an icon)
+    cd ../../../
+    rm -rf .DS_Store
+    ln -s /Applications .
+    cp ../../res/icon.icns .VolumeIcon.icns
+    SetFile -c icnC .VolumeIcon.icns
     cd $WORKDIR
+    hdiutil create -srcfolder output/OSX/ -volname "BennuGD Packager" -format UDRW -ov "output/raw.dmg"
+    rm -rf output/OSX/
+    mkdir -p output/OSX/
+    hdiutil attach "output/raw.dmg" -mountpoint output/OSX/
+    SetFile -a C output/OSX/
+    hdiutil detach output/OSX/
+    rm -rf output/OSX/
+    hdiutil convert "output/raw.dmg" -format UDZO -o "$OUTPUTDMG"
+    rm -f "output/raw.dmg"
+#hdiutil create -fs HFS+ -volname "BennuGD Packager" -srcfolder output/OSX/ "$OUTPUTDMG"
 }
 
 # Determine if we're on Linux or on Mac and set output dir
@@ -31,6 +47,7 @@ function package_osx {
 PLATFORM=$(uname)
 if [ "$PLATFORM" = 'Darwin' ]; then
     OUTPUTDIR='output/OSX'
+    OUTPUTDMG='output/BennuGD Packager.dmg'
 else
     OUTPUTDIR='output/linux'
 fi
@@ -39,6 +56,10 @@ fi
 # Remove any previous output before creating the new one
 if [ -d "$OUTPUTDIR" ]; then
     rm -rf $OUTPUTDIR
+fi
+
+if [ -f "$OUTPUTDMG" ]; then
+    rm -f "$OUTPUTDMG"
 fi
 
 # Package the program
