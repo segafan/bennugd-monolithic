85f129255bab49a08cd4d2d9ef9613162577106a
josebagar@gmail.com <josebagar@gmail.com@f91b80c7-bb6a-0374-6d28-0ee0fdcfb287>
2012-08-21 00:37:02 +0000

    * PAck the Win32 binary so that empty directories are copied, too

--- packager/pack_packager.bat
+++ packager/pack_packager.bat
@@ -11,8 +11,8 @@ REM Package the program
 CALL c:\Python27\Scripts\cxfreeze.bat main.py --target-dir="output\win32" --base-name="Win32GUI" --include-modules="atexit,PyQt4.QtCore,PyQt4.QtGui,sqlite3,os,sys,uuid,shutil,subprocess" --icon="res/icon.ico"
 
 REM Copy the templates to the output dir
-XCOPY templates output\win32\templates /s /i
-XCOPY res output\win32\res /s /i
+XCOPY templates output\win32\templates /E /I
+XCOPY res output\win32\res /E /I
 DEL output\win32\res\icon.icns
 DEL output\win32\res\BennuGD_Packager.tar.bz2
 MOVE output\win32\main.exe "output\win32\BennuGD Packager.exe"
