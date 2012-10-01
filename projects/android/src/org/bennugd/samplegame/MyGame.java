bdf1dfc66a0ac6775116b657b3268bedafb9a4dc
josebagar <josebagar@gmail.com>
2012-10-01 15:22:34 +0000

    Now you can rename your app!

---
+++ projects/android/src/org/bennugd/samplegame/MyGame.java
@@ -0,0 +1,18 @@
+package org.bennugd.samplegame;
+
+import org.libsdl.app.SDLActivity;
+import android.os.*;
+
+/*
+ * A sample wrapper class that just calls SDLActivity
+ */ 
+
+public class MyGame extends SDLActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
