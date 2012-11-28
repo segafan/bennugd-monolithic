34b50bc9cf55e863a98c97fb15bcd141792502fc
joseba.gar@gmail.com <joseba.gar@gmail.com@f91b80c7-bb6a-0374-6d28-0ee0fdcfb287>
2012-11-28 13:43:08 +0000

    * Changes

--- packager/ui_preferences.py
+++ packager/ui_preferences.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'ui_preferences.ui'
 #
-# Created: Mon Oct  1 17:58:06 2012
-#      by: PyQt4 UI code generator 4.9.3
+# Created: Wed Nov 28 13:20:56 2012
+#      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -17,9 +17,9 @@ except AttributeError:
 class Ui_Preferences(object):
     def setupUi(self, Preferences):
         Preferences.setObjectName(_fromUtf8("Preferences"))
-        Preferences.resize(475, 122)
+        Preferences.resize(475, 153)
         self.layoutWidget = QtGui.QWidget(Preferences)
-        self.layoutWidget.setGeometry(QtCore.QRect(10, 10, 451, 101))
+        self.layoutWidget.setGeometry(QtCore.QRect(10, 10, 451, 134))
         self.layoutWidget.setObjectName(_fromUtf8("layoutWidget"))
         self.verticalLayout = QtGui.QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setMargin(0)
@@ -37,6 +37,30 @@ class Ui_Preferences(object):
         self.button_SDKPath.setObjectName(_fromUtf8("button_SDKPath"))
         self.horizontalLayout_5.addWidget(self.button_SDKPath)
         self.verticalLayout.addLayout(self.horizontalLayout_5)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.labelkeystore = QtGui.QLabel(self.layoutWidget)
+        self.labelkeystore.setObjectName(_fromUtf8("labelkeystore"))
+        self.horizontalLayout.addWidget(self.labelkeystore)
+        self.linekeystore = QtGui.QLineEdit(self.layoutWidget)
+        self.linekeystore.setEnabled(False)
+        self.linekeystore.setStatusTip(_fromUtf8(""))
+        self.linekeystore.setObjectName(_fromUtf8("linekeystore"))
+        self.horizontalLayout.addWidget(self.linekeystore)
+        self.button_keystore = QtGui.QPushButton(self.layoutWidget)
+        self.button_keystore.setObjectName(_fromUtf8("button_keystore"))
+        self.horizontalLayout.addWidget(self.button_keystore)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_3 = QtGui.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.labelkeystorepass = QtGui.QLabel(self.layoutWidget)
+        self.labelkeystorepass.setObjectName(_fromUtf8("labelkeystorepass"))
+        self.horizontalLayout_3.addWidget(self.labelkeystorepass)
+        self.linekeystorepass = QtGui.QLineEdit(self.layoutWidget)
+        self.linekeystorepass.setEchoMode(QtGui.QLineEdit.Password)
+        self.linekeystorepass.setObjectName(_fromUtf8("linekeystorepass"))
+        self.horizontalLayout_3.addWidget(self.linekeystorepass)
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
         self.buttonBox = QtGui.QDialogButtonBox(self.layoutWidget)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
@@ -53,4 +77,8 @@ class Ui_Preferences(object):
         self.labelSDK.setText(QtGui.QApplication.translate("Preferences", "Android SDK dir:", None, QtGui.QApplication.UnicodeUTF8))
         self.lineSDK.setPlaceholderText(QtGui.QApplication.translate("Preferences", "Please choose path to SDK", None, QtGui.QApplication.UnicodeUTF8))
         self.button_SDKPath.setText(QtGui.QApplication.translate("Preferences", "Choose...", None, QtGui.QApplication.UnicodeUTF8))
+        self.labelkeystore.setText(QtGui.QApplication.translate("Preferences", "KeyStore:", None, QtGui.QApplication.UnicodeUTF8))
+        self.linekeystore.setPlaceholderText(QtGui.QApplication.translate("Preferences", "[Optional] Please choose path to Android Keystore", None, QtGui.QApplication.UnicodeUTF8))
+        self.button_keystore.setText(QtGui.QApplication.translate("Preferences", "Choose...", None, QtGui.QApplication.UnicodeUTF8))
+        self.labelkeystorepass.setText(QtGui.QApplication.translate("Preferences", "KeyStore password:", None, QtGui.QApplication.UnicodeUTF8))
 
