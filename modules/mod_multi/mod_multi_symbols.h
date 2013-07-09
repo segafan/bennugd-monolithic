ef92396ec3d287a661ce860ac692d27d510d96a5
josebagar <josebagar@gmail.com>
2013-07-09 20:30:57 +0000

    mod_key is now compiling. Only God knows if this works at all......

--- modules/mod_multi/mod_multi_symbols.h
+++ modules/mod_multi/mod_multi_symbols.h
@@ -1,5 +1,5 @@
 /*
-*  Copyright (c) 2011 Joseba García Echebarria. All rights reserved.
+*  Copyright (C) 2013 Joseba García Etxebarria. All rights reserved.
 *
 *  This software is provided 'as-is', without any express or implied
 *  warranty. In no event will the authors be held liable for any damages
