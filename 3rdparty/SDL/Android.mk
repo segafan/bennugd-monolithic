a48357e4dde9008df1843aa9b4d56308b9675e78
josebagar <josebagar@gmail.com>
2013-10-30 20:21:22 +0000

    Random stuff while updating to SDL 2.0.1

--- 3rdparty/SDL/Android.mk
+++ 3rdparty/SDL/Android.mk
@@ -35,6 +35,7 @@ LOCAL_SRC_FILES := \
 	$(wildcard $(LOCAL_PATH)/src/loadso/dlopen/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/power/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/power/android/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/filesystem/dummy/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/render/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/render/*/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/stdlib/*.c) \
@@ -49,3 +50,20 @@ LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES
 LOCAL_LDLIBS := -ldl -lGLESv1_CM -lGLESv2 -llog -landroid
 
 include $(BUILD_SHARED_LIBRARY)
+
+###########################
+#
+# SDL static library
+#
+###########################
+
+LOCAL_MODULE := SDL2_static
+
+LOCAL_MODULE_FILENAME := libSDL2
+
+LOCAL_SRC_FILES += $(LOCAL_PATH)/src/main/android/SDL_android_main.c
+
+LOCAL_LDLIBS := 
+LOCAL_EXPORT_LDLIBS := -Wl,--undefined=Java_org_libsdl_app_SDLActivity_nativeInit -ldl -lGLESv1_CM -lGLESv2 -llog -landroid
+
+include $(BUILD_STATIC_LIBRARY)
--- 3rdparty/SDL/test/testfilesystem.c
+++ 3rdparty/SDL/test/testfilesystem.c
@@ -9,32 +9,25 @@
   including commercial applications, and to alter it and redistribute it
   freely.
 */
-
-/* Print out all the scancodes we have, just to verify them */
+/* Simple test of power subsystem. */
 
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "SDL.h"
 
 int
 main(int argc, char *argv[])
 {
-    SDL_Scancode scancode;
-
     /* Enable standard application logging */
     SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s\n", SDL_GetError());
-        exit(1);
+    if (SDL_Init(0) == -1) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init() failed: %s\n", SDL_GetError());
+        return 1;
     }
-    for (scancode = 0; scancode < SDL_NUM_SCANCODES; ++scancode) {
-        SDL_Log("Scancode #%d, \"%s\"\n", scancode,
-               SDL_GetScancodeName(scancode));
-    }
+
+    SDL_Log("base path: '%s'\n", SDL_GetBasePath());
+    SDL_Log("pref path: '%s'\n", SDL_GetPrefPath("libsdl", "testfilesystem"));
+
     SDL_Quit();
-    return (0);
+    return 0;
 }
